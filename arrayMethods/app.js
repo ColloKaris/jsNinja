4e4dd82e5fdfba9bb29cc6122f8b8a4bcb512297
Collins Kariuki <collinsmunkary@gmail.com>
2022-12-27 08:06:59 +0300

    Chaining array methods

--- arrayMethods/app.js
+++ arrayMethods/app.js
@@ -139,3 +139,18 @@ players.sort((a,b) => {
 })
 
 console.log(players)
+
+
+//CHAINING ARRAY METHODS
+const product = [
+    {name: 'gold star', price: 30},
+    {name: 'green shell', price: 10},
+    {name: 'red shell', price: 40},
+    {name: 'banana skin', price: 5},
+    {name: 'mushroom', price: 50}
+];
+
+const promos = products
+    .filter(product => product.price > 20)
+    .map((product) => `the ${product.name} is ${product.price /2} pounds`)
+console.log(promos)
