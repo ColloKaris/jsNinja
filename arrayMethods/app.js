06a60057b538b610a99188dbc809e8dedcddc442
Collins Kariuki <collinsmunkary@gmail.com>
2022-12-27 06:47:07 +0300

    Reduce Method

--- arrayMethods/app.js
+++ arrayMethods/app.js
@@ -1,4 +1,4 @@
-const scores = [10,30, 15, 25, 50, 40, 5]
+// const scores = [10,30, 15, 25, 50, 40, 5]
 
 // const filteredScores = scores.filter((score) => {
 //     return score > 20;
@@ -46,4 +46,43 @@ const saleProducts = products.map((product) => {
     }
 })
 
-console.log(saleProducts);
+// console.log(saleProducts);
+
+//Reduce method
+//const scores = [10,20, 60, 40, 70, 90, 30];
+
+// const result = scores.reduce((acc, curr) => {
+//     if(curr > 50){
+//         acc++;
+//     }
+//     return acc;
+// }, 0);
+
+// console.log(result);
+
+const scores = [
+    {player: 'mario', score:50},
+    {player: 'yoshi', score:30},
+    {player: 'mario', score:70},
+    {player: 'crystal', score:60},
+    {player: 'mario', score:50},
+    {player: 'yoshi', score:30},
+    {player: 'mario', score:70},
+    {player: 'crystal', score:60},
+    {player: 'mario', score:50},
+    {player: 'yoshi', score:30},
+    {player: 'mario', score:70},
+    {player: 'crystal', score:60},
+    {player: 'mario', score:50},
+    {player: 'yoshi', score:30},
+    {player: 'mario', score:70},
+    {player: 'crystal', score:60}
+];
+
+const marioTotal = scores.reduce((acc,curr) => {
+    if(curr.player === 'mario'){
+        acc += curr.score;
+    }
+    return acc;
+}, 0);
+console.log(marioTotal);
