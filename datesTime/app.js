158e26fc59310f591caaa60039dffd26f2bb14dd
Collins Kariuki <collinsmunkary@gmail.com>
2022-12-29 17:33:52 +0300

    Dates and time with a few functions

---
+++ datesTime/app.js
@@ -0,0 +1,13 @@
+// dates & times
+const now = new Date();
+console.log(now);
+console.log(typeof now);
+
+// year, months, day, times
+console.log('getFullYear:', now.getFullYear());
+console.log('getMonth:', now.getMonth());
+console.log('getDate:', now.getFullYear());
+console.log('getDay:', now.getDay());
+console.log('getHours:', now.getHours());
+console.log('getMinutes:', now.getMinutes());
+console.log('getSeconds:', now.getSeconds());
