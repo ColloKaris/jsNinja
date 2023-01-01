6e582ae307252b055120e044148167083744b3ef
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-01 18:20:20 +0300

    timestamps and date strings

--- datesTime/app.js
+++ datesTime/app.js
@@ -11,3 +11,11 @@ console.log('getDay:', now.getDay());
 console.log('getHours:', now.getHours());
 console.log('getMinutes:', now.getMinutes());
 console.log('getSeconds:', now.getSeconds());
+
+// timestamps
+console.log('timestamp:', now.getTime())
+
+// date strings
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toLocaleString());
