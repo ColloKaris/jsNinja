20ef266b59705f628ff305ddf1beed1d498fce15
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-03 07:18:41 +0300

    Adding DateFns library to use some of its methods such as format to format the dates

---
+++ datesTime/sandbox.js
@@ -0,0 +1,11 @@
+//create a javascript date
+const now = new Date();
+
+//console.log(dateFns.isToday(now));
+
+//formatting options
+console.log(dateFns.format(now,'YYYY'));
+console.log(dateFns.format(now,'MMMM'));
+console.log(dateFns.format(now,'dddd'));
+console.log(dateFns.format(now,'Do'));
+console.log(dateFns.format(now,'dddd Do MMMM YYYY'))
