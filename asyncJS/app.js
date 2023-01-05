fb8c992313c909440ba884a7c6eeac162be8eed7
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-05 23:07:00 +0300

    comments on async JS

--- asyncJS/app.js
+++ asyncJS/app.js
@@ -1,11 +1,10 @@
-console.log(1);
-console.log(2);
+//to make request, we first create a request object
+const request = new XMLHttpRequest();
 
-//this is an examples of asynchronous code
-//it does not block the execution of code
-setTimeout(() => {
-    console.log('callback function fired')
-}, 2000);
+request.open('GET', )
 
-console.log(3);
-console.log(4);
+
+// Javascript is synchronous by nature allowing the execution 
+// of only a single statement at a time
+// Through asynchrounous JS, the browser takes a request and handles it outside 
+// of  the scope of the single thread allowing other tasks to go on
