e0915e658d4ad1feda5c6191c4dd1b2fc1460fa0
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-06 17:08:43 +0300

    JSON Data

--- asyncJS/app.js
+++ asyncJS/app.js
@@ -6,16 +6,19 @@ const getTodos = (callback) => {
 
 request.addEventListener('readystatechange', () => {
     //console.log(request, request.readyState)
+    
     if(request.readyState === 4 && request.status === 200){
         //console.log(request, request.responseText);
-        callback(undefined, request.responseText);
+        const data = JSON.parse(request.responseText);
+        callback(undefined, data);
     } else if(request.readyState === 4){
         //console.log('could not fetch the data')
         callback('could not fetch data', undefined);
     }
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
+//https://jsonplaceholder.typicode.com/todos/
+request.open('GET', 'todos.json');
 request.send();
 };
 
