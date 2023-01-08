18ecd350a81cd04e7f648df3df8e6583f0379a91
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-08 21:00:57 +0300

    comments on asynchronous code

--- coltAsync/app.js
+++ coltAsync/app.js
@@ -4,3 +4,10 @@ setTimeout(() => {
     console.log("Here is your date from the server....")
 }, 3000);
 console.log("I AT THE END OF THE FILE!");
+
+//Asynchronous javascript relies on a special partof the browser to handle
+// the request
+// it makes uses of web apis, javascripts identifies web api functions and passes
+//the to the web api 
+//browsers are written in languages such as C++ and can do what 
+//javascript can't. That is why they can support multithreading
