131e6cd6d1689fd2b294ad2becbe6b23ea0a6d73
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-10 21:25:07 +0300

    promises intro

--- coltAsync/app.js
+++ coltAsync/app.js
@@ -5,15 +5,5 @@ setTimeout(() => {
 }, 3000);
 console.log("I AT THE END OF THE FILE!");
 
-//Asynchronous javascript relies on a special partof the browser to handle
-// the request
-// it makes uses of web apis, javascripts identifies web api functions and passes
-//the to the web api 
-//browsers are written in languages such as C++ and can do what 
-//javascript can't. That is why they can support multithreading
-
-//This is more comments on this post
-// I will make a timetable tomorrow and have a more structured day
-//where I target one technical article a day instead of overwhelming
-//myself Then I'll ensure I limit my smartphone use and implement the pomodoro
-//technique
+//today I went through 2 tutorials on udemy covering callback hell
+//and promises
