82058cdfc3420c18bcb1adefc18830ed8f9e0804
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-14 19:57:01 +0300

    Sato

--- coltAsync/app.js
+++ coltAsync/app.js
@@ -4,11 +4,4 @@ setTimeout(() => {
     console.log("Here is your date from the server....")
 }, 3000);
 console.log("I AT THE END OF THE FILE!");
-
-//today I went through 2 tutorials on udemy covering callback hell
-//and promises
-
-//Jan 11 spent over 4 hours trying to solve and error on Linux, still didn't
-//work but I am hopeful I will resolve it tomorrow whe nI am fresh
-
-//wasted Friday but still wanted to commit, I'll make up for it tomorrow
+ //again I couldn't concentrate
