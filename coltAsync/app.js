f54416fcac592cff53148ac0ebe0fe848f5e9284
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-16 18:11:06 +0300

    promises & the magic of promises on udemy

--- coltAsync/app.js
+++ coltAsync/app.js
@@ -4,5 +4,10 @@ setTimeout(() => {
     console.log("Here is your date from the server....")
 }, 3000);
 console.log("I AT THE END OF THE FILE!");
- //again I couldn't concentrate
- //tomorrow I will go through async javascript
+
+//covered two vidoes on promises on udemy
+//promises make codes less deeply nested through promise chaining
+//the trick in making promise chaining work is returning a promise 
+//within our callback then we have one catch statement to handle
+//al the rejected requests
+
