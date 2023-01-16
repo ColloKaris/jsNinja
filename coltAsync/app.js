f6d319363953ef3ce3525506e62af095c8e9d6f9
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-16 18:18:16 +0300

    modification for date 14 January

--- coltAsync/app.js
+++ coltAsync/app.js
@@ -1,5 +1,7 @@
 console.log("Sending request to server!");
-
+//this comment was added to test whether I can make a commit
+//to an earlier date on github. The commit is to be made on the 14th of Jan
+//which I missed committing for lawd knows why
 setTimeout(() => {
     console.log("Here is your date from the server....")
 }, 3000);
