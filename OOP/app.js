847fa587450a4f9e326ede6ee48e1401a11b6805
Collins Kariuki <collinsmunkary@gmail.com>
2023-01-30 16:02:22 +0300

    class methos and method chaining

--- OOP/app.js
+++ OOP/app.js
@@ -5,10 +5,36 @@ class User {
         //set up properties on the object
         this.username = username;
         this.email = email;
+        this.score = 0;
     }
+    login(){
+        console.log(`${this.username} just logged in`);
+        return this;
+    }
+
+    logout(){
+        console.log(`${this.username} just logged out`)
+        return this;
+    }
+
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username} has a score of ${this.score}`);
+        return this;
+    }
+
 }
 
-const userOne =new User('mario', 'mario@gmail.com');
-const userTwo =new User('luigi', 'luigi@gmail.com');
+const userOne = new User('mario', 'mario@gmail.com');
+const userTwo = new User('luigi', 'luigi@gmail.com');
 console.log(userOne);
-console.log(userTwo)
+console.log(userTwo);
+
+// userOne.logout();
+// userTwo.login();
+
+// userOne.incScore();
+// userOne.incScore();
+
+//METHOD CHAINING
+userOne.login().incScore().incScore().logout();
