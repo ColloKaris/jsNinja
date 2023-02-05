ce74f0e8596a5c1f4c0e0b37ce8379a652a3bd90
Collins Kariuki <collinsmunkary@gmail.com>
2023-02-05 06:05:18 +0300

    NPM Scrips & Watching Files

---
+++ babelWebpack/src/index.js
@@ -0,0 +1,14 @@
+const greet = name => {
+    console.log(`hello ${name}`);
+};
+
+greet('mario');
+greet('luigi');
+greet('link');
+
+
+class User {
+    constructor(){
+        this.score = 0;
+    }
+}
